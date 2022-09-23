f8a8831e15aecac6cb8f8328e2720221a0b7a145
ashistiwari2 <66716885+ashistiwari2@users.noreply.github.com>
2022-09-23 10:43:57 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-# gunicorn -w 4 -k uvicorn.workers.UvicornWorker Logistic_api.main:app
-gunicorn Logistic_api.main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8443 --timeout 600
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker Logistic_api.main:app --timeout 900
+#gunicorn Logistic_api.main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8443 --timeout 
 pip install python-multipart
 
