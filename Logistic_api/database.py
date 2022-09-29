0d3a10e0c7fde640a0b21e4d4a81fcf7498b7592
ashistiwari2 <66716885+ashistiwari2@users.noreply.github.com>
2022-09-29 18:00:32 +0530

    Update database.py

--- Logistic_api/database.py
+++ Logistic_api/database.py
@@ -4,17 +4,18 @@ from sqlalchemy.orm import sessionmaker
 import os
 import urllib
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./logistic.db"
-host_server = os.environ.get('host_server', ' 127.0.0.1')
-db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '3306')))
-database_name = os.environ.get('database_name', 'fastapi_app')
-db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'ashistiwari2')))
-db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', '1234567890@At')))
-SQLALCHEMY_DATABASE_URL='mysql+mysqlconnector://{}:{}@{}:{}/{}'.format(db_username,db_password,host_server,db_server_port,database_name)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./logistic.db"
+# host_server = os.environ.get('host_server', ' 127.0.0.1')
+# db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '3306')))
+# database_name = os.environ.get('database_name', 'fastapi_app')
+# db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'ashistiwari2')))
+# db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', '1234567890@At')))
+# SQLALCHEMY_DATABASE_URL='mysql+mysqlconnector://{}:{}@{}:{}/{}'.format(db_username,db_password,host_server,db_server_port,database_name)
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,pool_size=3, max_overflow=0
-    #connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    #pool_size=3, max_overflow=0
+    connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
